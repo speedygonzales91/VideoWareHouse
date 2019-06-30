a0b70c373298c27c2045553d112aed69ecd13f91
Zsolt Molnár <mozb17@gmail.com>
2019-06-30 18:24:50 +0200

    CustomerController is fixed with _context. View is adjusted.

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -13,27 +13,31 @@ namespace VideoProject.Controllers
 {
     public class CustomersController : Controller
     {
-        public static List<Customer> customerList = new List<Customer>()
-            {
-                new Customer() {Id = 0, Name = "John Smith"},
-                new Customer() {Id = 1, Name="Mary Williams"}
-            };
-        public static Movie movie = new Movie() { Id = 0, Name = "Shrek" };
+        private ApplicationDbContext _context;
 
-        RandomMovieViewModel randomMovie = new RandomMovieViewModel() { Movie = movie, Customers = customerList };
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         // GET: Customers
         public ActionResult Index()
         {
-            return View(randomMovie);
+            var customers = _context.Customers.ToList();
+            return View(customers);
         }
 
         public ActionResult Details(int id)
         {
 
-            if (id < randomMovie.Customers.Count)
+            if (id < _context.Customers.ToList().Count)
             {
-                var customer = randomMovie.Customers[id];
+                var customer = _context.Customers.SingleOrDefault(x => x.Id == id);
                 return View(customer);
             }
             return View("NoCustomer");

cb4bee075eb8427cd8c54dce99d6b94ddbdb590f
Zsolt Molnár <mozb17@gmail.com>
2019-06-30 18:11:48 +0200

    Add dataannotations to Customer

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,9 +1,13 @@
-﻿namespace VideoProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoProject.Models
 {
     public class Customer
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string  Name { get; set; }
 
         public bool IsSubscribedToNewsLetter { get; set; }

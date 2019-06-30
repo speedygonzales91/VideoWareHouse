186794e87dd196ac4b6761af619e1389fda6a1d8
Zsolt Molnár <mozb17@gmail.com>
2019-06-30 18:07:14 +0200

    Add migrations + membershipTypes model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,5 +5,12 @@
         public int Id { get; set; }
 
         public string  Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        //Navigation Property
+        public MembershipType MembershipType { get; set; }
+        //Foreign key
+        public byte MembershipTypeId { get; set; }
     }
 }

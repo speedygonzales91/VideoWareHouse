d77e4989999bd814d1edbeef3ee471d8bc10f630
mozsolt <zsolt.molnar@fx.hu>
2019-07-02 10:46:01 +0200

    Add Genre + Movie properties

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,9 +1,21 @@
-﻿namespace VideoProject.Models
+﻿using System;
+
+namespace VideoProject.Models
 {
     public class Movie
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public byte NumberInStock { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public int GenreId { get; set; }
     }
 }

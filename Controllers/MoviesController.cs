a2eb4948ab4aca905de6c0c7f28ab99ffa0a5284
mozsolt <zsolt.molnar@fx.hu>
2019-07-11 11:22:24 +0200

    Section 4: Finish exercise

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -48,10 +48,11 @@ namespace VideoProject.Controllers
         }
 
         // GET Movies/Edit/
-        public ActionResult Edit(int id)
+       /* public ActionResult Edit(int id)
         {
             return Content($"id={id}");
         }
+        */
         // GET: Movies
         //public ActionResult Index(int? pageIndex, string sortBy)
         //{
@@ -73,6 +74,7 @@ namespace VideoProject.Controllers
             return Content($"year={year},month={month}");
         }
 
+        
         public ActionResult Details (int id)
         {
             if (id <= _context.Movies.ToList().Count)
@@ -82,6 +84,54 @@ namespace VideoProject.Controllers
                 return View(movie);
             }
             return HttpNotFound();
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var movieViewModel = new MovieFormViewModel()
+            {
+                Genres = genres
+            };
+            return View("MovieSave", movieViewModel);
+        }
+        [System.Web.Http.HttpPost]
+        public ActionResult Save (Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.DateAdded = movie.DateAdded;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");      
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                var editViewModel = new MovieFormViewModel()
+                {
+                    Movie = movie,
+                    Genres = _context.Genres.ToList()
+                };
+                return View("MovieSave", editViewModel);
+            }
         }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace VideoProject.Models
 {
@@ -8,14 +9,18 @@ namespace VideoProject.Models
 
         public string Name { get; set; }
 
+        [Display(Name = "Date of Released")]
         public DateTime? ReleaseDate { get; set; }
 
+        [Display(Name = "Date of Addition")]
         public DateTime? DateAdded { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
 
         public Genre Genre { get; set; }
 
+        [Display(Name = "Genre")]
         public int GenreId { get; set; }
     }
 }

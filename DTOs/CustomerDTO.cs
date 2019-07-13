0144ce10aec3a70307ea4fc085d0c1b86c0f506c
Zsolt Molnár <mozb17@gmail.com>
2019-07-13 22:12:45 +0200

    Add CustomerDTO to project

---
+++ DTOs/CustomerDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VideoProject.ValidationAttributes;
+
+namespace VideoProject.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIsAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}

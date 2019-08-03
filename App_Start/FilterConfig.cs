8011bab7a328371b5c0760360d025d5da7960aff
Zsolt Molnár <mozb17@gmail.com>
2019-08-03 15:33:00 +0200

    Section8: Restricting access for controllers. Allow HomeController only.

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace VideoProject
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute()); //this will apply for all controller
         }
     }
 }

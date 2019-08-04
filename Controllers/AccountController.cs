0cbf7e6d10cf66982b21e6da799c85d53fdbde33
Zsolt Molnár <mozb17@gmail.com>
2019-08-04 09:10:36 +0200

    Section8: Add new profile data: DrivingLicense to model and controller.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -152,18 +152,22 @@ namespace VideoProject.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    DrivingLicense = model.DrivingLicense
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-
+                    /* ONLY NECESSARY FOR ADMIN USER - run only once
                     //Temp Code
                     var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
                     var roleManager = new RoleManager<IdentityRole>(roleStore);
                     await roleManager.CreateAsync(new IdentityRole("CanManageMovies"));
 
                     await UserManager.AddToRoleAsync(user.Id, "CanManageMovies");
-
+                    */
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
